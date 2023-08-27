008f25c9a0e208d45fcbd86181b43cd95321543f
SudarshanTimalsina <121552853+SudarshanTimalsina@users.noreply.github.com>
2023-08-27 15:57:16 +0545

    Update logic.js

--- logic.js
+++ logic.js
@@ -1,38 +1,13 @@
-function generateString(length) {
-  const characters =   "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  let result = '';
-  const Charlen = characters.length;
-
-  for (let i = 0; i < length; i++) {
-    const random = Math.floor(Math.random() * Charlen);
-    result += characters.charAt(random);
+function check(){
+ const input = document.getElementById("ip").value;
+  let  aa  = input.toLowerCase()
+  if(aa === "fyauri"){
+    alert ("Correct!")
   }
-
-  return result;
-}
-
-function logic() {
-    const rl = document.getElementById("url").value;
-  const url =rl.toLowerCase()
-  const check = url.includes("www.") && url.includes(".com");
-if (check === true){
-  const backlink = generateString(6);
-  const shortenedUrl = "https://www.sudarshantimalsina.com.np/" + backlink;
-document.getElementById("output").textContent = shortenedUrl
-}
-else{
-  alert("please provide a valid web address!")
-}}
-
-function prank() {
-  const lr = document.getElementById("url").value;
-  const abc =lr.toLowerCase()
-  const chk = abc.includes("www.") && abc.includes(".com");
-if (chk === true){
- const pr="https://www.sudarshantimalsina.com.np/xvgch"
-document.getElementById("gms").textContent = pr
+  else  {
+    alert ("Sorry wrong answer!")
   }
-else{
-  alert("please provide a valid web address!")
-}}
-
+} 
+function hint(){
+alert("Send NRS.50 at Esewa ID: 9765416344")
+}
